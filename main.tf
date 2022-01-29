33c116e9c9224cbd2aeefe1638115db5ca2ac364
Ryan Wholey <ryan@takescoop.com>
2022-01-28 18:25:28 -0700

    Add provider source and version

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ data "okta_everyone_group" "everyone" {}
 output "everyone" {
   value = data.okta_everyone_group.everyone
 }
+
+
+terraform {
+  required_version = ">= 1, < 2"
+
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "3.19.0"
+    }
+  }
+}
